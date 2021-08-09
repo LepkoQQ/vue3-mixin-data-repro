9cc5654a89da9780eafb51dfd2aa33c13179988f
LepkoQQ <lepko.san@gmail.com>
2021-08-09 19:35:37 +0200

    Initial commit (working repro)

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>
+    fromComponent: {{ fromComponent }}
+  </p>
+  <p>
+    fromMixin: {{ fromMixin }}
+  </p>
+  <p>
+    length (this.fromMixin.length accessed in component data() func): {{ length }}
+  </p>
+</template>
+
+<script>
+import testMixin from './test-mixin'
+
+export default {
+  mixins: [testMixin],
+  data() {
+    return {
+      fromComponent: "COMP_DATA",
+      length: this.fromMixin.length,
+    }
+  }
+}
+</script>
